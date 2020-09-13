62d628f3e6ad4c0d2b6d07592e8f3cea55ca21e1
msguttman <msguttman@gmail.com>
2020-09-12 22:46:21 -0500

    cors cors cors

--- config/application.rb
+++ config/application.rb
@@ -30,9 +30,27 @@ module Flatnote
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => '*'
-    }
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+         origins '*'
+         resource '*', :headers => :any, :methods => [:get, :post, :options]
+       end
+    end
+    
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*',
+    #       :headers => :any,
+    #       :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+    #       :max_age => 0
+    #   end
+    # end
+
+    # config.action_dispatch.default_headers = {
+    # 'Access-Control-Allow-Origin' => '*'
+    # }
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
